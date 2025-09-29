64b30d3469f6924de28c4100bc837cbc39334854
Teodorooh <165227645+Teodorooh@users.noreply.github.com>
2025-09-28 23:25:49 -0300

    Create perfil_teste_1_web.pl

---
+++ testes/perfil_teste_1_web.pl
@@ -0,0 +1,11 @@
+% Perfil focado em DESENVOLVIMENTO WEB
+resposta_nota(1,2).
+resposta_nota(2,2).
+resposta_nota(3,5).  % unipolar web
+resposta_nota(4,2).
+resposta_nota(5,5).  % (seguranca ou web) -> Y=web  => +2 web
+resposta_nota(6,3).  % neutro
+resposta_nota(7,3).  % neutro
+resposta_nota(8,5).  % (IA ou web) -> Y=web => +2 web
+resposta_nota(9,3).  % neutro
+resposta_nota(10,3). % neutro
