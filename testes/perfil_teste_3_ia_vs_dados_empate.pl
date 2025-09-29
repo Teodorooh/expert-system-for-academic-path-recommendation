77c34e198342e433dae9fcff4f5174111ff853e8
Teodorooh <165227645+Teodorooh@users.noreply.github.com>
2025-09-28 23:26:59 -0300

    Create perfil_teste_3_ia_vs_dados_empate.pl

---
+++ testes/perfil_teste_3_ia_vs_dados_empate.pl
@@ -0,0 +1,11 @@
+% Perfil equilibrado entre IA e CIENCIA DE DADOS (empate)
+resposta_nota(1,5).  % +1 dados
+resposta_nota(2,5).  % +1 IA
+resposta_nota(3,2).
+resposta_nota(4,2).
+resposta_nota(5,3).  % neutro
+resposta_nota(6,2).  % (dados ou seguranca) -> +1 dados
+resposta_nota(7,3).  % neutro
+resposta_nota(8,3).  % neutro
+resposta_nota(9,4).  % (dados ou IA) -> +1 IA
+resposta_nota(10,3). % neutro
