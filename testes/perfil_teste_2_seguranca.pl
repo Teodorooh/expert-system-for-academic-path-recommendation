c0dff1ae04c287081ae98f7d94aeb3e902d518b3
Teodorooh <165227645+Teodorooh@users.noreply.github.com>
2025-09-28 23:26:31 -0300

    Create perfil_teste_2_seguranca.pl

---
+++ testes/perfil_teste_2_seguranca.pl
@@ -0,0 +1,11 @@
+% Perfil focado em SEGURANCA DA INFORMACAO
+resposta_nota(1,2).
+resposta_nota(2,2).
+resposta_nota(3,2).
+resposta_nota(4,3).
+resposta_nota(5,1).  % (seguranca ou web) -> X=seguranca => +2 seg
+resposta_nota(6,5).  % (dados ou seguranca) -> Y=seguranca => +2 seg
+resposta_nota(7,3).  % neutro
+resposta_nota(8,3).  % neutro
+resposta_nota(9,3).  % neutro
+resposta_nota(10,5). % (redes ou seguranca) -> Y=seguranca => +2 seg
